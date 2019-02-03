5e06aed7fbad5c06a1ffb1d9f63f8c2cd7fbdc3e
Marios Vasileiou <2248900v@student.gla.ac.uk>
2019-02-03 21:12:03 +0000

    CH_9_complete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -86,10 +86,17 @@ DATABASES = {
     }
 }
 
+# LOGIN_URL = '/rango/login'
+LOGIN_URL = '/rango/login_redirect'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
